4a8ef7d6b46d0841e42771e28890ae410f8fdcea
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-04 03:03:03 -0300

    inserting images

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header = () =>{
+    return (
+        <header className='center'>
+            <h1>Iniciando o Projeto</h1>
+        </header>
+    );
+}
+
+export default Header;
