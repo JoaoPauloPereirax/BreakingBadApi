8c71f7dfba91ebc548b4fb6ad96c745c7b97131e
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2020-12-04 07:32:35 -0300

    Connecting the API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState,useEffect} from 'react';
 import './App.css';
-import Header from './components/Header';
+import CharacterGrid from './components/characters/CharacterGrid';
+import Header from './components/ui/Header';
+//import CharacterGrid from './components/characters/CharacterGrid';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(()=>{
+    const fetchItems = async ()=>{
+      const result = await axios(`https://www.breakingbadapi.com/api/characters`)
+      setItems(result.data);
+      console.log(result.data);
+      setIsLoading(false);
+    }
+    
+    fetchItems();
+
+  },[]);
+
  return (
    <div className="container">
      <Header/>
