5888579dfd975c55836f7c08bd7dbb265a1622cc
João Paulo Pereira <joaopaulomatematicapho@gmail.com>
2021-01-12 20:18:24 -0300

    Console.log

--- src/pages/Character/index.js
+++ src/pages/Character/index.js
@@ -1,38 +1,30 @@
-import React, {useEffect, useState} from 'react';
-import {useParams} from 'react-router-dom'
-import Api from '../../services/Api'
-import CharacterItem from '../../components/CharacterItem'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Api from "../../services/Api";
+import CharacterItem from "../../components/CharacterItem";
 
-function Character (){
+function Character() {
+  const params = useParams();
+  const userId = params.id;
 
-  const params = useParams()
-  const userId = params.id
+  const [item, setItem] = useState({});
 
-  const [item,setItem] = useState({})
+  useEffect(() => {
+    const fetchItem = async () => {
+      const result = await Api.get(`/${userId}`);
+      const character = result.data[0];
+      setItem(character);
+      console.log(result.data);
+    };
 
-  useEffect(()=>{
-    const fetchItem = async ()=>{
-      const result = await Api.get(`/${userId}`)
-      const character = result.data[0]
-      setItem(character)
-      //console.log(result.data)
-      
-    }
-
-    fetchItem()
-
-  },[userId])
-    
+    fetchItem();
+  }, [userId]);
 
   return (
-  <section className="card">
-    {
-        <CharacterItem key={item.char_id} item={item}></CharacterItem>
-    }
-  </section>
-  )
-
+    <section className="card">
+      {<CharacterItem key={item.char_id} item={item}></CharacterItem>}
+    </section>
+  );
 }
 
-
-export default Character
+export default Character;
